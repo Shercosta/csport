9a36a1e93e4f73c6a66d0e1f0468ded5c9e6c3d4
BilhaqAD07 <bilhaqcr7@gmail.com>
2023-05-08 09:02:25 +0700

    Updating Profile n EditProfile

--- App.js
+++ App.js
@@ -1,22 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
-import Splash from "./components/Splash";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+// import Splash from "./components/Splash";
+// import SignIn from "./src/signIn";
+// import SignUp from "./src/signUp";
+import ProfileScreen from "./src/Profile/ProfileScreen";
+import EditProfile from "./src/Profile/EditProfile";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      <Splash />
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Edit Profile"
+          component={EditProfile}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    // <Text>Open up App.js to start working on your app!</Text>
+    //  <Splash />
+    //   <SignIn />
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#7642AC",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
