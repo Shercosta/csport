61d7846fcbb38273b514b045a17fecd6674f9b18
Shercosta <70560582+Shercosta@users.noreply.github.com>
2023-05-08 19:50:04 +0700

    attempt 2

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 import Splash from "./components/Splash";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ProfileScreen from "./components/ProfileScreen";
+import EditProfile from "./components/EditProfile";
+import SplashScreen from "react-native-splash-screen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.js to start working on your app!</Text> */}
-      {/* <Splash /> */}
-      <ProfileScreen />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        {/* <Text>Open up App.js to start working on your app!</Text> */}
+        <Splash />
+        {/* <ProfileScreen /> */}
+        {/* <EditProfile /> */}
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
