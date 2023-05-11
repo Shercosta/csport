c73e3550b31b7366ce818e5834b5642349626c70
Shercosta <70560582+Shercosta@users.noreply.github.com>
2023-05-11 21:07:20 +0700

    merging

--- App.js
+++ App.js
@@ -1,42 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-<<<<<<< Updated upstream
 import { StyleSheet, Text, View, Image } from "react-native";
 import Splash from "./components/Splash";
 import * as React from "react";
-=======
-import { StyleSheet, Text, View } from "react-native";
-import Home from "expo/AppEntry";
-// import Todo from "./src/Todo/todo";
-import ProfileScreen from "./src/Profile/ProfileScreen";
-import EditProfile from "./src/Profile/EditProfile";
-import Leaderboard from "./src/Run/Run";
-import Timer from "./src/Run/Timer";
-import SignIn from "./src/signIn";
-import SignUp from "./src/signUp";
-
-// import Settings from "./src/settings";
->>>>>>> Stashed changes
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ProfileScreen from "./components/ProfileScreen";
 import EditProfile from "./components/EditProfile";
 import SplashScreen from "react-native-splash-screen";
 
-<<<<<<< Updated upstream
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <View style={styles.container}>
-        {/* <Text>Open up App.js to start working on your app!</Text> */}
-        <Splash />
-        {/* <ProfileScreen /> */}
-        {/* <EditProfile /> */}
-        <StatusBar style="auto" />
-      </View>
-    </NavigationContainer>
-=======
 // import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -74,7 +45,6 @@ export default function App() {
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
->>>>>>> Stashed changes
   );
 }
 
